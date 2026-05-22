2d1df257e23d621faa9aa4e15219ff55c6c7281a
Daffa Yusranizar Arrifi <daffa28.dn@gmail.com>
2026-05-23 01:22:00 +0800

    Initial commit

---
+++ team-10-c3/team_10_c3App.swift
@@ -0,0 +1,17 @@
+//
+//  team_10_c3App.swift
+//  team-10-c3
+//
+//  Created by Daffa Yuranizar Arrifi on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct team_10_c3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
