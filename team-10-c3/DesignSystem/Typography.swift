2ef89c1bbdbeeba92d22dd67e535399af03076c2
HuyTran2720 <ht2005790@gmail.com>
2026-05-27 10:23:46 +0800

    feat: heading typography

--- team-10-c3/DesignSystem/Typography.swift
+++ team-10-c3/DesignSystem/Typography.swift
@@ -5,3 +5,40 @@
 //  Created by Huy Tran on 26/05/26.
 //
 // [P1] Font scale tokens
+
+import SwiftUI
+
+extension Font {
+    // MARK: Headings
+    static var heading1: Font {
+        .system(size: 64)
+            .weight(.semibold)
+    }
+    
+    static var heading2: Font {
+        .system(size: 54)
+            .weight(.semibold)
+    }
+    
+    static var heading3: Font {
+        .system(size: 44)
+            .weight(.semibold)
+    }
+    
+    static var heading4: Font {
+        .system(size: 34)
+            .weight(.semibold)
+    }
+    
+    static var heading5: Font {
+        .system(size: 26)
+            .weight(.semibold)
+    }
+    
+    static var heading6: Font {
+        .system(size: 20)
+            .weight(.semibold)
+    }
+    
+    // MARK: Body
+}
